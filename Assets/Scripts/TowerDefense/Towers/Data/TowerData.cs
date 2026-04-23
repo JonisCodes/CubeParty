0601080f6a3f2ae127998ae2038041713be2d0cc
Missfoster <jontehojo@live.se>
2026-04-23 13:09:00 +0200

    Rest of unstaged files

---
+++ Assets/Scripts/TowerDefense/Towers/Data/TowerData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using TowerDefense.Abilities;
+using TowerDefense.Targeting;
+using UnityEngine;
+
+namespace TowerDefense.Towers.Data
+{
+    [CreateAssetMenu(fileName = "towerData", menuName = "Tower/Data", order = 0)]
+    public class TowerData : ScriptableObject
+    {
+        public float damage;
+        public float baseRange;
+        [Tooltip("Attacks per second")] public float attackSpeed;
+        public List<Ability> abilities;
+        public TargetingStrategy targeting;
+    }
+}
